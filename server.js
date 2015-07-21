dbed82e40ac1fb3cb4636707a971ce6bedcd59f7
diggedydog <eric@mcweiner.org>
2015-07-21 11:30:38 -0400

    Test

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var application = require('express')();
 var http = require('http').Server(application);
+/*
 var io = require('socket.io').listen(http);
 var port = 3000;
 
 
 io.on('connection', function (socket) {
     socket.on('testConnect', function (info) {
-        socket.emit("WOW IT WORKED")
+        socket.emit("WOW IT WORKED");
     });
            
 });
-
+*/
 http.listen(port, function () {
     console.log('[LocateFriends] Listening on *:' + port);
 });
